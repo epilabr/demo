ce32ce3bedef5b920ff2c7386096f693c8e39d70
itamar <epilabr@walla.co.il>
2019-10-21 19:37:23 +0300

    Update run_container_script.py

--- run_container_script.py
+++ run_container_script.py
@@ -1,2 +1,2 @@
 import subprocess
-subprocess.call(["docker", "run", "--name", "demoContainer", "-p", "8080:8080", "demo"])
+subprocess.call(["docker", "run", "-d", "--name", "demoContainer", "-p", "8080:8080", "demo"])
